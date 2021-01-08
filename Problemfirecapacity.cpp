84f1d377c296f6f8c9288a5c3675e8e10d12b604
Ian Hakeman <hakeman@chapman.edu>
2021-01-08 01:44:37 +0000

    Adds Problemfirecapacity.cpp

---
+++ Problemfirecapacity.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;//used for cin and cout
+
+int main(int argc, char **argv) {
+    unsigned short maxRoomCapacity;//short is plenty big for attendant size for a room, unsigned since always positive
+    unsigned short numPeopleAttending;//short also plenty long enough for number of people attending a meeting
+    short diffCapacityAndAttending;//difference between number of people allowed and number actually attending
+
+    cout << "Maximum room capacity: ";
+    cin >> maxRoomCapacity;
+
+    cout << "Number of people to attend the meeting: ";
+    cin >> numPeopleAttending;
+
+    diffCapacityAndAttending = maxRoomCapacity - numPeopleAttending;//positive if capacity exceeds those attending
+
+    if (diffCapacityAndAttending >= 0) {//if difference is positive or zero, it is legal
+        cout << "It is legal to hold the meeting." << endl;
+        cout << diffCapacityAndAttending << " more people can legally attend." << endl;
+    }
+    else {
+        cout << "The meeting cannot be held as planned due to fire regulations." << endl;
+        cout << -diffCapacityAndAttending << " people must be excluded in order to meet the fire regulations." << endl;//negative difference since the value of the variable is negative if it got to this code block
+    }
+
+    return 0;
+}
